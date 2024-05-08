34c324b7e3c8f68f4e591f503bf97fa25c43079b
Shivam Bhardwaj <Shivambhardwaj8585@gmail.com>
2024-05-08 20:42:20 +0530

    modified middleware authMiddleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  ignoredRoutes: ["/api/webhook"],
+  publicRoutes: ["/api/webhook"],
 });
 
 export const config = {
