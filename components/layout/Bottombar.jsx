ba1af00fc8810bcf81fcde98030c7e4ca0c51d27
Shivam Bhardwaj <Shivambhardwaj8585@gmail.com>
2024-05-08 17:57:21 +0530

    Created Home Layout

---
+++ components/layout/Bottombar.jsx
@@ -0,0 +1,31 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const Bottombar = () => {
+  const pathname = usePathname();
+  return (
+    <div className="flex bottom-0 z-20 w-full bg-dark-1 px-6 py-3 items-center justify-between md:hidden">
+      {sidebarLinks.map((link) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <Link
+            key={link.label}
+            href={link.route}
+            className={`flex gap-2 items-center rounded-lg py-2 px-4 ${
+              isActive && "bg-purple-1"
+            }`}
+          >
+            {link.icon}
+            <p className="text-light-1 text-small-medium max-sm:hidden">{link.label}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Bottombar;
---
+++ components/layout/Topbar.jsx
@@ -0,0 +1,55 @@
+"use client";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { Add, Logout, Search } from "@mui/icons-material";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const Topbar = () => {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  return (
+    <div className="flex justify-between items-center mt-6">
+      <div className="relative">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search posts, people, ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Search className="search-icon" onClick={() => {}} />
+      </div>
+      <button
+        className="create-post-btn"
+        onClick={() => router.push("/create-post")}
+      >
+        <Add />
+        <p>Create a post</p>
+      </button>
+
+      <div className="flex gap-3">
+        <SignedIn>
+          <SignOutButton>
+            <div className="flex cursor-pointer items-center md:hidden">
+              <Logout sx={{ color: "white", fontSize: "32px" }} />
+            </div>
+          </SignOutButton>
+        </SignedIn>
+
+        <Link href="/">
+          <Image
+            src="/assets/mithudon2.jpg"
+            alt="profile-photo"
+            width={50}
+            height={50}
+            className="rounded-full md:hidden"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
